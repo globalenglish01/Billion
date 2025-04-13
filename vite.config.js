9d89320f283e3b23658bf37f71f4a225670cf0d3
globalenglish01 <65394674+globalenglish01@users.noreply.github.com>
2025-04-13 19:19:46 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Billion/', // 一定要是你的仓库名，区分大小写
   plugins: [react()],
-  base: '/Billion/', // 仓库名
-});
+})
