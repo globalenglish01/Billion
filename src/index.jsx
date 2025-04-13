4c32745d04669e2d510b0315333b681fa17018ca
globalenglish01 <65394674+globalenglish01@users.noreply.github.com>
2025-04-13 17:26:32 +0900

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles.css";
-import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "./styles.css";
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
